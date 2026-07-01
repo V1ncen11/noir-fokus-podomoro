a7373a05915d783b7fb36b4c550b317e7f47c015
V1ncen11 <kevin10222075@sttcipasung.ac.id>
2026-07-01 23:54:22 +0700

    Fix Serwist v9 package integration

--- app/sw.ts
+++ app/sw.ts
@@ -1,14 +1,14 @@
 import { defaultCache } from "@serwist/next/worker";
-import type { PrecacheEntry } from "@serwist/precaching";
-import { Serwist } from "@serwist/sw";
+import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
+import { Serwist } from "serwist";
 
 declare global {
-  interface WorkerGlobalScope extends ServiceWorkerGlobalScope {
+  interface WorkerGlobalScope extends SerwistGlobalConfig {
     __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
   }
 }
 
-declare const self: WorkerGlobalScope;
+declare const self: ServiceWorkerGlobalScope;
 
 const serwist = new Serwist({
   precacheEntries: self.__SW_MANIFEST,
